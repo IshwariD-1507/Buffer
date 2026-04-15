d313ece7d82c700918fda2b752b9ec7e2e7ce51e
IshwariD-1507 <ishwaristudy15@gmail.com>
2026-04-16 01:03:47 +0530

    Imported weight and astar

--- run.py
+++ run.py
@@ -1,44 +1,56 @@
 from graph.loader import load_city_graph
-graph= load_city_graph()
+from graph.dijkstra import dijkstra
+from graph.weights import apply_composite_weights
+from graph.astar import astar
+from map.render import plot_route
+
+import osmnx as ox
+
+# -----------------------
+# Load graph ONCE
+# -----------------------
+graph = load_city_graph()
 print("Graph loaded successfully!")
 
-
-from graph.dijkstra import dijkstra
-import osmnx as ox
-
-graph = load_city_graph()
-
-# # Example: pick random nodes
-nodes = list(graph.nodes)
-start = nodes[0]
-end = nodes[100]
-
-path, dist = dijkstra(graph, start, end)
-
-print("Distance:", dist)
-print("Path length:", len(path))
-
-from map.render import plot_route
-import osmnx as ox
-
-graph = load_city_graph()
-
-# Convert place to nearest node
-start_point = (18.53, 73.86)     #pune example change later
+# -----------------------
+# Coordinates (your example)
+# -----------------------
+start_point = (18.53, 73.86)
 end_point = (18.5208, 73.8554)
 
+# Convert to nearest nodes
 start_node = ox.distance.nearest_nodes(graph, start_point[1], start_point[0])
 end_node = ox.distance.nearest_nodes(graph, end_point[1], end_point[0])
 
-# # Run Dijkstra
-path, dist = dijkstra(graph, start_node, end_node)
+# -----------------------
+# Apply composite weights
+# -----------------------
+graph = apply_composite_weights(graph, mode="Rush Hour")
 
-print("Distance:", dist)
+# -----------------------
+# Run Dijkstra (with weight)
+# -----------------------
+path_dij, dist_dij = dijkstra(graph, start_node, end_node, weight='weight')
 
-# # Plot map
-m = plot_route(graph, path)
+# -----------------------
+# Run A* (IMPORTANT: 3 outputs)
+# -----------------------
+path_astar, dist_astar, explored = astar(graph, start_node, end_node, weight='weight')
 
-# # Save map
+# -----------------------
+# Comparison
+# -----------------------
+print("\n===== COMPARISON =====")
+print("Dijkstra Distance:", dist_dij)
+print("A* Distance:", dist_astar)
+print("A* Nodes Explored:", explored)
+print("Dijkstra Path Length:", len(path_dij))
+
+# -----------------------
+# Plot A* route
+# -----------------------
+m = plot_route(graph, path_astar)
+
+# Save map
 m.save("route.html")
-
 print("Map saved as route.html")
